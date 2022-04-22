009df11b6a7cdc054d6dd094a272e955ba42d3eb
Wiktoria <wiktoria@MacBook-Air-Wiktoria.local>
2022-04-22 20:48:52 +0200

    added people NavLink

--- star-wars/src/components/Navigation.jsx
+++ star-wars/src/components/Navigation.jsx
@@ -13,7 +13,7 @@ const Navigation = () => {
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
 						<Nav.Link as={NavLink} end to="/films">Films</Nav.Link>
-						{/* <Nav.Link as={NavLink} to="/people">People</Nav.Link> */}
+						<Nav.Link as={NavLink} to="/people">People</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
